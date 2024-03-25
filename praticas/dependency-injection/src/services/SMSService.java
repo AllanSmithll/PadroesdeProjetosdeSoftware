108b2d5d46b5fd346b20cd0654b55dba01402c40
AllanSmithll <101681192+AllanSmithll@users.noreply.github.com>
2024-03-24 21:45:23 -0300

    Retirando comentários

--- praticas/dependency-injection/src/services/SMSService.java
+++ praticas/dependency-injection/src/services/SMSService.java
@@ -10,7 +10,6 @@ import interfaces.MessageService;
 public class SMSService implements MessageService {
 
 	public void sendMessage(String message, String receiver){
-		// Neste ponto teríamos a lógica (conjunto de instruções) para enviar um email
 		System.out.println("SMS sent to "+receiver+ " with Message="+message);
 	}
 }
