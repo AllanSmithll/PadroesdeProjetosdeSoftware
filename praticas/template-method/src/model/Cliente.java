2b8a1550d06bee091d230428e62dd57a43b450b0
Allan Amancio <allanammancio2004@gmail.com>
2024-07-07 23:20:28 -0300

    Atividade de template method com deleções

--- praticas/template-method/src/model/Cliente.java
+++ praticas/template-method/src/model/Cliente.java
@@ -25,39 +25,6 @@ public class Cliente {
         dvdsAlugados.add(aluguel);
     }
 
-    public String extrato() {
-        final String fimDeLinha = System.getProperty("line.separator");
-        Iterator<Aluguel> alugueis = dvdsAlugados.iterator();
-        String resultado = "Registro de Alugueis de " + getNome() + fimDeLinha;
-        while (alugueis.hasNext()) {
-            Aluguel cada = alugueis.next();
-            resultado += "\t" + cada.getDVD().getTitulo() + "\t R$ " +
-                    cada.calcularValor() + fimDeLinha;
-        }
-        resultado += "Valor total pago: R$ " + getValorTotal() + fimDeLinha;
-        resultado += "Voce acumulou " + getPontosTotaisDeAlugadorFrequente() +
-                " pontos de alugador frequente";
-        return resultado;
-    }
-
-    public String extratoHTML() {
-        final String fimDeLinha = System.getProperty("line.separator");
-        Iterator<Aluguel> alugueis = dvdsAlugados.iterator();
-        String resultado = "<H1>Registro de Alugueis de <EM>" +
-                getNome() + "</EM></H1><P>" + fimDeLinha;
-        while (alugueis.hasNext()) {
-            Aluguel cada = alugueis.next();
-            resultado += cada.getDVD().getTitulo() + ": R$ " +
-                    cada.calcularValor() + "<BR>" + fimDeLinha;
-        }
-        resultado += "<P>Valor total pago: <EM>R$ " + getValorTotal() +
-                "</EM>" + fimDeLinha;
-        resultado += "<P>Voce acumulou <EM>" +
-                getPontosTotaisDeAlugadorFrequente() +
-                " pontos </EM> de alugador frequente";
-        return resultado;
-    }
-
     public double getValorTotal() {
         double total = 0;
         for (Aluguel aluguel : dvdsAlugados) {
